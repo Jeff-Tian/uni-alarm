0f644de8c2816229e279350d0d2686117cfbd8c0
Jeff Tian <jie.tian@unicareer.com>
2019-07-24 19:07:24 +0800

    :label: workable version of typescript

--- nwb.config.js
+++ nwb.config.js
@@ -2,6 +2,20 @@ module.exports = {
   type: 'react-component',
   npm: {
     esModules: true,
-    umd: false
-  }
+    umd: false,
+  },
+  webpack: {
+    config(config) {
+      config.entry = {
+        demo: ['./demo/src/index.tsx'],
+      }
+      config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx']
+      config.module.rules.push({
+        'test': /\.tsx?$/,
+        'loader': 'awesome-typescript-loader',
+      })
+
+      return config
+    },
+  },
 }
---
+++ src/alarm.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export default class UniAlarm extends Component {
+    render() {
+        return <div>
+            <h2>UniAlarm</h2>
+        </div>
+    }
+}
